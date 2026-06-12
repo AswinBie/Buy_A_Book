cb2e26c3ea7d172e0999f4fed72e660829d76f36
AswinGovindan <AswinGovindan@QTS-LAP-042>
2026-06-12 04:30:04 -0400

    Cosmetical_Changes_11_06_2026

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./pages/login/login') },
   { path: 'register', loadComponent: () => import('./pages/register/register') },
   {
